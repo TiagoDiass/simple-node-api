6e7d7f5f598d14a028ec6e430018d06718398ad9
Tiago Dias <tiago.costadiasss@gmail.com>
2020-05-03 15:47:04 -0300

    Rotas

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,19 @@ routes.get('/', (req, res) => {
     res.redirect('/patients');
 });
 
+// Listagem de todos os pacientes
 routes.get('/patients', patients.all);
 
+// Adição de um novo paciente
 routes.post('/patients/new', patients.new);
 
+// Mostrar um paciente específico
 routes.get('/patients/:id', patients.show);
 
+// Alteração dos dados de um paciente
 routes.put('/patients/:id', patients.edit);
+
+// Exclusão de um paciente
+routes.delete('/patients/:id', patients.delete);
 
 module.exports = routes;
